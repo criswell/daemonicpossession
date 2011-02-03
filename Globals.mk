064ef5399506d9af6bc9b64702f111e305175982
Sam Hart <criswellious@gmail.com>
2011-02-03 14:29:14 -0800

    * Silly reStructuredText... I don't get it. Go with what I know. * Basic global makefile include

---
+++ Globals.mk
@@ -0,0 +1,15 @@
+# Global makefile settings
+
+DEBUG=-g -D 'DEBUG'
+
+CC=gcc
+CPP=g++
+
+INSTALL=install
+
+CFLAGS=-c -Wall -Wno-write-strings -Wno-parentheses
+CP_FLAGS=-c -Wall -Wno-write-strings -Wno-parentheses
+
+STATIC_BASE=-static
+STATIC_CC=-static-libgcc
+STATIC_CPP=-static-libstdc++
